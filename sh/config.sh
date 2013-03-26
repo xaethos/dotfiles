3b39f7bb9bbf655fd929e67ad3b38f13568bd595
Diego García <diego@xaethos.net>
2013-03-26 08:58:39 +0000

    Clean up config.sh

--- sh/config.sh
+++ sh/config.sh
@@ -1,12 +1,5 @@
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export PS1='%m:%3~$(git_info_for_prompt)%# '
-# else
-#   export PS1='%3~$(git_info_for_prompt)%# '
-# fi
-
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-HISTSIZE=10000
-SAVEHIST=10000
-
+export HISTSIZE=10000
+export SAVEHIST=10000
