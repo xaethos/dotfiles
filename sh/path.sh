55e31730f08c577f0ce0372a89b06d9d8faa264a
Diego García <diego@xaethos.net>
2013-03-26 05:51:56 +0000

    Better PATH

--- sh/path.sh
+++ sh/path.sh
@@ -1,3 +1,20 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$HOME/bin:$PATH:$DOTHOME/bin:$HOME/.rvm/bin"
-export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
+NEW_PATH=""
 
+append_path() {
+  if [ -d $1 ]
+  then
+    NEW_PATH="$NEW_PATH:$1"
+  fi
+}
+
+append_path "/usr/local/bin"
+append_path "/usr/local/sbin"
+append_path "$HOME/bin"
+append_path "$DOTHOME/bin"
+
+NEW_PATH="$NEW_PATH:$PATH"
+
+append_path "$HOME/.rvm/bin"
+
+export PATH=${NEW_PATH#:}
+export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
