ce4531db000e7279bb92b69942c6da9d30c5ad9f
Diego García <diego@xaethos.net>
2013-03-26 08:59:00 +0000

    Better path.bash

--- sh/path.sh
+++ sh/path.sh
@@ -1,20 +1,22 @@
-NEW_PATH=""
-
-append_path() {
+function append_path() {
   if [ -d $1 ]
   then
-    NEW_PATH="$NEW_PATH:$1"
+    PATH="$PATH:$1"
   fi
 }
+
+__OLD_PATH=$PATH
+PATH=""
 
 append_path "/usr/local/bin"
 append_path "/usr/local/sbin"
 append_path "$HOME/bin"
 append_path "$DOTHOME/bin"
 
-NEW_PATH="$NEW_PATH:$PATH"
+PATH="$PATH:$__OLD_PATH"
+unset __OLD_PATH
 
 append_path "$HOME/.rvm/bin"
 
-export PATH=${NEW_PATH#:}
+export PATH=${PATH#:}
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
