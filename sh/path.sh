ec2b5a06cb20ba393f71890aa65366a35ce2435f
Diego García <xaethos@gmail.com>
2015-06-03 14:44:25 +0800

    Add Python and Homebrew to PATH

--- sh/path.sh
+++ sh/path.sh
@@ -8,16 +8,30 @@ function append_path() {
 __OLD_PATH=$PATH
 PATH=""
 
+
+## Python stuff
+append_path "/Library/Frameworks/Python.framework/Versions/3.4/bin"
+
+## Local binaries
 append_path "/usr/local/bin"
 append_path "/usr/local/sbin"
 append_path "$HOME/bin"
 append_path "$DOTHOME/bin"
 
+## Homebrew
+if [ -d $HOMEBREW_PREFIX ]
+then
+  append_path "$HOMEBREW_PREFIX/bin"
+fi
+
+## Old PATH
 PATH="$PATH:$__OLD_PATH"
 unset __OLD_PATH
 
-append_path "$HOME/.rvm/bin"
+## RVM
+#append_path "$HOME/.rvm/bin"
 
+## Android
 if [ -d $ANDROID_HOME ]
 then
   append_path "$ANDROID_HOME/tools"
