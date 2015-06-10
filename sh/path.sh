e95779d19cf4d6e3d53bbedc65c6d5baafeb075a
Diego García <xaethos@gmail.com>
2015-06-10 21:17:53 +0800

    Remove Python PATH
    clean up path.sh while at it

--- sh/path.sh
+++ sh/path.sh
@@ -9,9 +9,6 @@ __OLD_PATH=$PATH
 PATH=""
 
 
-## Python stuff
-append_path "/Library/Frameworks/Python.framework/Versions/3.4/bin"
-
 ## Local binaries
 append_path "/usr/local/bin"
 append_path "/usr/local/sbin"
@@ -19,30 +16,18 @@ append_path "$HOME/bin"
 append_path "$DOTHOME/bin"
 
 ## Homebrew
-if [ -d $HOMEBREW_PREFIX ]
-then
-  append_path "$HOMEBREW_PREFIX/bin"
-fi
+append_path "$HOMEBREW_PREFIX/bin"
 
 ## Old PATH
 PATH="$PATH:$__OLD_PATH"
 unset __OLD_PATH
 
-## RVM
-#append_path "$HOME/.rvm/bin"
-
 ## Go
-if [ -d $GOPATH ]
-then
-  append_path "$GOPATH/bin"
-fi
+append_path "$GOPATH/bin"
 
 ## Android
-if [ -d $ANDROID_HOME ]
-then
-  append_path "$ANDROID_HOME/tools"
-  append_path "$ANDROID_HOME/platform-tools"
-fi
+append_path "$ANDROID_HOME/tools"
+append_path "$ANDROID_HOME/platform-tools"
 
 export PATH=${PATH#:}
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
